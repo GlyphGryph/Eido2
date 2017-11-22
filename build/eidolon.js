aa33937ab9df548870920a4f63be8f3004d66003
GlyphGryph <glyphgryph@gmail.com>
2017-11-22 08:05:44 -0500

    Setup npm and babel transpiler and build chain

---
+++ build/eidolon.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var gameInterval;
+var gameCanvas;
+var eventCatcherDiv;
+
+function startLoading() {
+  eventCatcherDiv = document.getElementById("EventCatcher");
+  // eventCatcherDiv events go here
+
+  gameCanvas = document.getElementById("GraphicsBox");
+
+  gameInterval = setInterval(hasLoaded, 250);
+}
+
+function hasLoaded() {
+  if (true) {
+    clearInterval(gameInterval);
+    startGame();
+  }
+}
+
+function startGame() {
+  gameInterval = setInterval(runGame, 25);
+}
+
+function runGame() {}
