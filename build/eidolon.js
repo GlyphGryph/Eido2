fc56f4817feecfc767788c2ac0f2fbc17a112afc
GlyphGryph <glyphgryph@gmail.com>
2017-11-22 08:15:30 -0500

    Got the current script building and running correctly, just a toy app for now though

--- build/eidolon.js
+++ build/eidolon.js
@@ -7,6 +7,7 @@ var eventCatcherDiv;
 function startLoading() {
   eventCatcherDiv = document.getElementById("EventCatcher");
   // eventCatcherDiv events go here
+  eventCatcherDiv.addEventListener("mousemove", canvasMove);
 
   gameCanvas = document.getElementById("GraphicsBox");
 
@@ -20,8 +21,25 @@ function hasLoaded() {
   }
 }
 
+var heroX = 10;
+var heroY = 10;
+function drawTheHero(g) {
+  g.fillStyle = "#0000FF";
+  g.fillRect(heroX, heroY, 20, 20);
+}
+
 function startGame() {
+  drawTheHero(gameCanvas.getContext("2d"));
   gameInterval = setInterval(runGame, 25);
 }
 
-function runGame() {}
+function canvasMove(E) {
+  E = E || window.event;
+  heroX = E.pageX;
+  heroY = E.pageY;
+}
+
+function runGame() {
+  gameCanvas.getContext("2d").clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+  drawTheHero(gameCanvas.getContext("2d"));
+}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ var eventCatcherDiv;
 function startLoading(){
   eventCatcherDiv = document.getElementById("EventCatcher");
   // eventCatcherDiv events go here
+  eventCatcherDiv.addEventListener("mousemove", canvasMove);
 
   gameCanvas = document.getElementById("GraphicsBox");
 
@@ -18,10 +19,25 @@ function hasLoaded(){
   }
 }
 
+var heroX = 10;
+var heroY = 10;
+function drawTheHero(g){
+    g.fillStyle = "#0000FF";
+    g.fillRect(heroX, heroY, 20, 20);
+}
+
 function startGame(){
-  gameInterval = setInterval(runGame, 25);
+    drawTheHero(gameCanvas.getContext("2d"));
+    gameInterval = setInterval(runGame, 25);
+}
+
+function canvasMove(E){
+    E = E || window.event;
+    heroX = E.pageX;
+    heroY = E.pageY;
 }
 
 function runGame(){
-
+    gameCanvas.getContext("2d").clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+    drawTheHero(gameCanvas.getContext("2d"));
 }
