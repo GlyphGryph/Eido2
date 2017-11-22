2630d73ba472f2b344d69c80e06c1ebe9c0cf4e3
GlyphGryph <glyphgryph@gmail.com>
2017-11-22 15:59:54 -0500

    Sort of working

--- src/index.js
+++ src/index.js
@@ -1,58 +1,15 @@
 import * as PIXI from 'pixi.js'
-var gameInterval;
-var gameCanvas;
-var eventCatcherDiv;
 
 function initialize(){
   //Create the renderer and stage
-  var renderer = PIXI.autoDetectRenderer(601, 401);
-  //var stage = new PIXI.Container();
+  let renderer = PIXI.autoDetectRenderer(601, 401)
+  let stage = new PIXI.Container();
 
   //Add the elements to the html
-  //document.getElementById('BackgroundBox').appendChild(renderer.view);
-  //renderer.render(stage);
+  document.getElementById('BackgroundBox').appendChild(renderer.view);
+  renderer.render(stage);
 
   //startLoading()
-}
-
-function startLoading(){
-  eventCatcherDiv = document.getElementById("EventCatcher");
-
-  // eventCatcherDiv events go here
-  eventCatcherDiv.addEventListener("mousemove", canvasMove);
-
-  //gameCanvas = document.getElementById("GraphicsBox");
-  //gameInterval = setInterval(hasLoaded, 250);
-}
-
-function hasLoaded(){
-  if(true){
-    clearInterval(gameInterval);
-    startGame();
-  }
-}
-
-var heroX = 10;
-var heroY = 10;
-function drawTheHero(g){
-  g.fillStyle = "#0000FF";
-  g.fillRect(heroX, heroY, 20, 20);
-}
-
-function startGame(){
-  drawTheHero(gameCanvas.getContext("2d"));
-  gameInterval = setInterval(runGame, 25);
-}
-
-function canvasMove(E){
-  E = E || window.event;
-  heroX = E.pageX;
-  heroY = E.pageY;
-}
-
-function runGame(){
-  gameCanvas.getContext("2d").clearRect(0, 0, gameCanvas.width, gameCanvas.height);
-  drawTheHero(gameCanvas.getContext("2d"));
 }
 
 initialize()
