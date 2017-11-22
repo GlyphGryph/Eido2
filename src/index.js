0218d095b8ff9340be064aff3e6d9126f9d662cc
GlyphGryph <glyphgryph@gmail.com>
2017-11-22 15:18:18 -0500

    Added Pixi and Browserify

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
+import PIXI from 'pixi.js'
 var gameInterval;
 var gameCanvas;
 var eventCatcherDiv;
 
+function initialize(){
+  //Create the renderer and stage
+  var renderer = PIXI.autoDetectRenderer(601, 401);
+  var stage = new PIXI.Container();
+
+  //Add the elements to the html
+  document.getElementById('BackgroundBox').appendChild(renderer.view);
+  renderer.render(stage);
+
+  startLoading()
+}
+
 function startLoading(){
   eventCatcherDiv = document.getElementById("EventCatcher");
+
   // eventCatcherDiv events go here
   eventCatcherDiv.addEventListener("mousemove", canvasMove);
 
-  gameCanvas = document.getElementById("GraphicsBox");
-
-  gameInterval = setInterval(hasLoaded, 250);
+  //gameCanvas = document.getElementById("GraphicsBox");
+  //gameInterval = setInterval(hasLoaded, 250);
 }
 
 function hasLoaded(){
@@ -22,22 +35,22 @@ function hasLoaded(){
 var heroX = 10;
 var heroY = 10;
 function drawTheHero(g){
-    g.fillStyle = "#0000FF";
-    g.fillRect(heroX, heroY, 20, 20);
+  g.fillStyle = "#0000FF";
+  g.fillRect(heroX, heroY, 20, 20);
 }
 
 function startGame(){
-    drawTheHero(gameCanvas.getContext("2d"));
-    gameInterval = setInterval(runGame, 25);
+  drawTheHero(gameCanvas.getContext("2d"));
+  gameInterval = setInterval(runGame, 25);
 }
 
 function canvasMove(E){
-    E = E || window.event;
-    heroX = E.pageX;
-    heroY = E.pageY;
+  E = E || window.event;
+  heroX = E.pageX;
+  heroY = E.pageY;
 }
 
 function runGame(){
-    gameCanvas.getContext("2d").clearRect(0, 0, gameCanvas.width, gameCanvas.height);
-    drawTheHero(gameCanvas.getContext("2d"));
+  gameCanvas.getContext("2d").clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+  drawTheHero(gameCanvas.getContext("2d"));
 }
