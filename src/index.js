d86396d4ee6f0bbd217153f043212dabce0e362d
GlyphGryph <glyphgryph@gmail.com>
2017-11-22 16:06:57 -0500

    removed some semicolons

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import * as PIXI from 'pixi.js'
 function initialize(){
   //Create the renderer and stage
   let renderer = PIXI.autoDetectRenderer(601, 401)
-  let stage = new PIXI.Container();
+  let stage = new PIXI.Container()
 
   //Add the elements to the html
-  document.getElementById('BackgroundBox').appendChild(renderer.view);
-  renderer.render(stage);
+  document.getElementById('BackgroundBox').appendChild(renderer.view)
+  renderer.render(stage)
 
   //startLoading()
 }
