974c2e5b666b97d47c59b28d4ca79c575e41be62
GlyphGryph <glyphgryph@gmail.com>
2018-01-20 18:37:46 -0500

    Loostacle sprites updated

--- build/eidolon.js
+++ build/eidolon.js
@@ -31784,14 +31784,14 @@ var Loostacle = function (_Obstacle) {
       if (this.active && this.checkCollisionWith(player)) {
         if (player.state.powerMode && player.state.goLeft) {
           console.log('obstacle eliminated');
-          this.manager.createRemnant(this.id + "_right_remnant", this.x, this.y, 10, -5, this.frames);
-          this.manager.createRemnant(this.id + "_left_remnant", this.x, this.y, 0, -10, this.frames);
+          this.manager.createRemnant(this.id + "_right_remnant", this.x, this.y, 10, -5, ["obstacles/brokenLoostacle/02"]);
+          this.manager.createRemnant(this.id + "_left_remnant", this.x, this.y, 0, -10, ["obstacles/brokenLoostacle/01"]);
           this.manager.remove(this.id);
         } else {
           console.log('ouch! obstacle hit');
           player.state.hitLoostacle = true;
-          this.manager.createRemnant(this.id + "_left_remnant", this.x - 5, this.y + 12, 0, 0, this.frames);
-          this.manager.createRemnant(this.id + "_right_remnant", this.x + 5, this.y + 15, 0, 0, this.frames);
+          this.manager.createRemnant(this.id + "_left_remnant", this.x - 5, this.y + 12, 0, 0, ["obstacles/brokenLoostacle/01"]);
+          this.manager.createRemnant(this.id + "_right_remnant", this.x + 5, this.y + 15, 0, 0, ["obstacles/brokenLoostacle/02"]);
           this.manager.remove(this.id);
         }
       }
